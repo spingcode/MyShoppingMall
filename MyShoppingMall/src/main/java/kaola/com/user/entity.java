d719ca8951b803d51dc3f7f6ccc32a98da01fbf6
zouw <2269607500@qq.com>
2018-01-26 19:35:14 +0800

    搭建web基本项目

---
+++ MyShoppingMall/src/main/java/kaola/com/user/entity.java
@@ -0,0 +1,16 @@
+package kaola.com.user;
+
+
+import lombok.Data;
+
+/**
+ * @Author:zouw
+ * @Description:
+ * @Date:Created in 18:41 2018/1/26
+ * @Modified By:
+ */
+@Data
+public class entity {
+
+
+}
